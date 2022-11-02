684d8d62bfff1de638ae46e013a435517da4696b
zakharfsk <fesikzakhar@khmnu.edu.ua>
2022-11-02 22:55:05 +0200

    change name project

--- MyResume/asgi.py
+++ MyResume/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Workshop_MyResume project.
+ASGI config for MyResume project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Workshop_MyResume.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyResume.settings')
 
 application = get_asgi_application()
--- MyResume/urls.py
+++ MyResume/urls.py
@@ -1,4 +1,4 @@
-"""Workshop_MyResume URL Configuration
+"""MyResume URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
